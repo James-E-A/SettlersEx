651fe288aaaf4434d5ff9239335a7e5b72bba6f6
James Edington Administrator <codegeek98@gmail.com>
2024-11-28 17:49:30 -0600

    new coordinates format

--- lib/settlersEx_web/view_util.ex
+++ lib/settlersEx_web/view_util.ex
@@ -26,89 +26,94 @@ defmodule SettlersView.Util do
 
   @inradius 96.0
   @half_sqrt_3 :math.sqrt(0.75)
-  def coords_array(:"point-top", shape) do
-    # include negative "index" for roads and tiles on the south-west edge
-    cols_range = -1..(shape.cols - 1);
-    rows_range = -1..(shape.rows - 1);
-    j_offset = 6; # FIXME
-    i_offset = -1.5; # FIXME
-    weave(
-      [rows_range, cols_range],
-      fn {j, i} ->
-        i_ = i - i_offset;
-        j_ = j_offset - j;
-        %{
-          x0: ((i_ - 0.5*j) + 0.5) * @inradius,
-          x_ww: (i_ - 0.5*j) * @inradius,
-          x_ee: ((i_ - 0.5*j) + 1) * @inradius,
-          x_e1: ((i_ - 0.5*j) + 0.75) * @inradius, # FIXME ESTIMATE
+  def coords_array(terrain, :"point-top") do
+    visible_tiles = Map.keys(terrain); # API guarantees no terrain tile is nil, so no need to filter
+    visible_edges =
+      visible_tiles
+      |> Stream.flat_map(fn {0,{i,j}} -> [{1,{i,j,0}}, {1,{i,j,1}}, {1,{i,j,2}}, {1,{i-1,j,0}}, {1,{i-1,j-1,1}}, {1,{i,j-1,2}}] end)
+      |> MapSet.new()
+    ;
+    visible_intersections =
+      visible_tiles
+      |> Stream.flat_map(fn {0,{i,j}} -> [{2,{i,j,0}}, {2,{i,j,1}}, {2,{i-1,j,0}}, {2,{i-1,j-1,1}}, {2,{i-1,j-1,0}}, {2,{i,j-1,1}}] end)
+      |> MapSet.new()
+    ;
 
-          y0: (j_ + 0.5) * @inradius * @half_sqrt_3,
-          y_n1: (j_ + 0.171875) * @inradius * @half_sqrt_3, # FIXME estimate
-          y_n2: (j_ - 0.078125) * @inradius * @half_sqrt_3, # FIXME estimate
-          y_n3: (j_ - 0.125) * @inradius * @half_sqrt_3, # FIXME estimate
-          y_nn: (j_ - 0.171875) * @inradius * @half_sqrt_3, # FIXME estimate
+    i_offset = Enum.min(Stream.map(visible_tiles, fn {0, {i, j}} -> (i - 0.5*j) end));
+    j_offset = Enum.max(Stream.map(visible_tiles, fn {0, {_i, j}} -> (j - 1) end))
 
-          y_ss: (j_ + 1.078125) * @inradius * @half_sqrt_3, # FIXME estimate
-          y_s3: (j_ + 1.078125) * @inradius * @half_sqrt_3, # FIXME estimate
-        }
-      end
-    )
-  end
-
-  defp weave([enumerable_1, enumerable_2], fun) do
-    enumerable_1
-    |> Stream.map(fn a -> {
-      a,
-      enumerable_2
-      |> Stream.map(fn b -> {
-        b,
-        fun.({a, b})
-      } end)
-      |> Map.new()
-    } end)
-    |> Map.new()
-  end
-
-  def viewBox(coords_array, terrain) do
-    stream_2d_map(coords_array)
-    |> Stream.filter(fn {j, i, _cur} -> (
-      j >= 0
-      and i >= 0
-      and not is_nil(terrain[j][i])
-    ) end)
-    |> Enum.reduce(nil, fn
-      {_j, _i, cur}, nil ->
-        %{
-          x_ww: cur.x_ww,
-          y_nn: cur.y_n3,
-          x_ee: cur.x_ee,
-          y_ss: cur.y_s3
-        }
-      {_j, _i, cur}, acc ->
-        %{
-          x_ww: min(acc.x_ww, cur.x_ww),
-          y_nn: min(acc.y_nn, cur.y_n3),
-          x_ee: max(acc.x_ee, cur.x_ee),
-          y_ss: max(acc.y_ss, cur.y_s3)
-        }
-    end)
-    |> (fn coords -> %{
-      "min-x": coords.x_ww,
-      "min-y": coords.y_nn,
-      width: coords.x_ee - coords.x_ww,
-      height: coords.y_ss - coords.y_nn,
-    } end).()
-  end
-
-  def stream_2d_map(m1) do
-    Stream.concat(
-      Stream.map(m1, fn {k1, m2} ->
-        Stream.map(m2, fn {k2, v} ->
-          {k1, k2, v}
+    %{
+      _style: :"point-top",
+      _offsets: %{i: i_offset, j: j_offset},
+      values: Map.new(Stream.concat([
+        Stream.map(visible_tiles, fn {0, {i, j}} ->
+          i_ = i - i_offset;
+          j_ = j_offset - j;
+          {
+            {0, {i, j}},
+            %{
+              x0: ((i_ - 0.5*j) + 0.5) * @inradius,
+              y0: (j_ + 0.5) * @inradius * @half_sqrt_3,
+            }
+          }
+        end),
+        Stream.map(visible_edges, fn {1, {i, j, k}} ->
+          i_ = i - i_offset;
+          j_ = j_offset - j;
+          {
+            {1, {i, j, k}},
+            %{
+              x0: ((i_ - 0.5*j) + case k do 0 -> 1; 1 -> 0.75; 2 -> 0.25 end) * @inradius, # FIXME estimate
+              y0: (j_ + case k do 0 -> 0.5; 1 -> 0; 2 -> 0 end) * @inradius * @half_sqrt_3,
+              rot: 90 - 60*k
+            }
+          }
+        end),
+        Stream.map(visible_intersections, fn {2, {i, j, k}} ->
+          i_ = i - i_offset;
+          j_ = j_offset - j;
+          {
+            {2, {i, j, k}},
+            %{
+              x0: ((i_ - 0.5*j) + case k do 0 -> 1; 1 -> 0.5 end) * @inradius,
+              y0: (j_ + case k do 0 -> 0.171875; 1 -> -0.171875 end) * @inradius * @half_sqrt_3, # FIXME estimate
+            }
+          }
         end)
-      end)
-    )
+      ]))
+    }
+  end
+
+  def viewBox(coords_array) do
+    {x_ww, y_nn, x_ee, y_ss} = case coords_array._style do
+      :"point-top" -> Enum.reduce(
+        Map.values(coords_array.values),
+        {0, 0, 0, 0},
+        fn
+          cur, {0, 0, 0, 0} ->
+            {
+              cur.x0,
+              cur.y0,
+              cur.x0,
+              cur.y0
+            }
+          cur, {x_ww, y_nn, x_ee, y_ss} ->
+            {
+              min(x_ww, cur.x0),
+              min(y_nn, cur.y0),
+              max(x_ee, cur.x0),
+              max(y_ss, cur.y0)
+            }
+        end
+      )
+    end
+
+    %{ # cast result to weird format required by SVG spec
+      "min-x": x_ww,
+      "min-y": y_nn,
+      width: x_ee - x_ww,
+      height: y_ss - y_nn,
+    }
   end
 
 end
